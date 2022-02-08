c3cbf7a4112e2b1dc08796a562b1937a3eba9594
Lagos77 <social_a@live.com>
2022-02-08 16:45:24 -0500

    User profile done

--- Recipess-app/UserProfile.swift
+++ Recipess-app/UserProfile.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct UserProfile: View {
+    
+    @State var username = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 70){
+            VStack{
+            VStack{
+                Image(systemName: "person.circle")
+                    .font(.system(size: 100))
+                    .padding(.horizontal, 10)
+            }
+                VStack{
+                    Text("Username")
+                        .font(.system(size: 30, weight: .bold))
+                        .padding(.horizontal, 10)
+                }
+                VStack(alignment: .leading){
+                    Text("Created recipes :")
+                        .font(.system(size: 20))
+                        .bold()
+                        .padding(.horizontal, 10)
+                    ScrollView{
+                        VStack{
+                            ForEach(0..<20) { index in Rectangle()
+                                    .fill(Color("ColorRed"))
+                                    .frame(height: 100)
+                            }
+                        }
+                    }
+                }
+                VStack(alignment: .leading){
+                    Text("Saved recipes :")
+                        .font(.system(size: 20))
+                        .bold()
+                        .padding(.horizontal, 10)
+                    ScrollView{
+                        VStack{
+                            ForEach(0..<20) { index in Rectangle()
+                                    .fill(Color.blue)
+                                    .frame(height: 100)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
