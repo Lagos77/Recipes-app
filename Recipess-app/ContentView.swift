7813a8640c2fd4b35396185d1c3bc1c4a082eff4
Lagos77 <social_a@live.com>
2022-02-08 10:57:56 -0500

    Initial Commit

---
+++ Recipess-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipess-app
+//
+//  Created by A J on 2022-02-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
